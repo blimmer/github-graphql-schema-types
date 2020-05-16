611a66f3b6d19b79eb7fe247c413137e1498b63f
Ben Limmer <ben@benlimmer.com>
2020-05-16 10:54:19 -0600

    Show examples of queries and mutations.

---
+++ src/index.ts
@@ -0,0 +1,68 @@
+import { githubClient } from "./client";
+import {
+  WhoAmIQuery,
+  WhoAmI,
+  GetRepoId,
+  GetRepoIdQuery,
+  GetRepoIdQueryVariables,
+  AddStarMutation,
+  AddStarMutationVariables,
+  AddStar,
+} from "./generated/graphql";
+
+async function whoAmI() {
+  const result = await githubClient().query<WhoAmIQuery>({
+    query: WhoAmI,
+  });
+
+  return result.data.viewer.login;
+}
+
+async function getBenLimmerDotComRepoId(): Promise<string> {
+  const result = await githubClient().query<
+    GetRepoIdQuery,
+    GetRepoIdQueryVariables
+  >({
+    query: GetRepoId,
+    variables: {
+      owner: "blimmer",
+      name: "benlimmer.com",
+    },
+  });
+
+  if (!result.data.repository) {
+    throw new Error(`Couldn't find repository id!`);
+  }
+
+  return result.data.repository.id;
+}
+
+async function starRepo(repoId: string) {
+  const result = await githubClient().mutate<
+    AddStarMutation,
+    AddStarMutationVariables
+  >({
+    mutation: AddStar,
+    variables: {
+      starrableId: repoId,
+    },
+  });
+
+  if (result.errors) {
+    throw new Error("Mutation failed!");
+  }
+
+  console.info(
+    `The repository now has ${result.data?.addStar?.starrable?.stargazers.totalCount} stargazers!!`
+  );
+}
+
+async function main() {
+  const username = await whoAmI();
+  console.info(`Your github username is ${username}`);
+
+  const benLimmerDotComRepoId = await getBenLimmerDotComRepoId();
+  await starRepo(benLimmerDotComRepoId);
+}
+
+main();
